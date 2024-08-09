80936f4eb9f9a842d343afc97b41a48ab2f49a24
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-08-09 15:21:39 -0300

    feat: function call to consult interest rate

--- src/gemini/gemini.model.js
+++ src/gemini/gemini.model.js
@@ -2,9 +2,39 @@ import {
   GoogleGenerativeAI,
   GenerativeModel,
   ChatSession,
+  FunctionDeclarationSchemaType
 } from "@google/generative-ai";
 import 'dotenv/config';
 import { GeminiEnum } from "./gemini.enum.js";
+
+export const functions = {
+  taxaJurosParcelamento: ({ value }) => {
+    const months = typeof value === 'string' ? parseInt(value) : value;
+    if (months <= 6) {
+      return 3;
+    } else if (months <= 12) {
+      return 5;
+    } else {
+      return 7;
+    }
+  }
+};
+
+const tools = {
+  functionDeclarations: [
+    {
+      name: 'taxaJurosParcelamento',
+      description: 'Retorna a taxa de juros para parcelamento baseado na quantidade de meses',
+      parameters: {
+        type: FunctionDeclarationSchemaType.OBJECT,
+        properties: {
+          value: { type: FunctionDeclarationSchemaType.NUMBER }
+        },
+        required: ['value'],
+      }
+    }
+  ]
+};
 
 /**
  * @param {string} name 
@@ -12,7 +42,7 @@ import { GeminiEnum } from "./gemini.enum.js";
  */
 export const getModel = (name = GeminiEnum["Gemini-1.0-pro"]) => {
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  return genAI.getGenerativeModel({ model: name, });
+  return genAI.getGenerativeModel({ model: name, tools });
 };
 
 /**
@@ -24,7 +54,8 @@ export const startChat = (model) => {
     history: [
       {
         role: 'user',
-        parts: [{ text: `
+        parts: [{
+          text: `
           Você é um chatbot que representa a empresa Jornada Viagens. Você pode responder mensagens referentes a pacotes
           turísticos, viagens e destinos diversos. Sempre peça o nome e e-mail do cliente antes de começar a responder
           as dúvidas.
--- src/gemini/gemini.service.js
+++ src/gemini/gemini.service.js
@@ -1,3 +1,5 @@
+import { functions } from "./gemini.model.js";
+
 class GeminiService {
   #model;
   #chat;
@@ -13,7 +15,29 @@ class GeminiService {
   async chat(userMessage) {
     const result = await this.#chat.sendMessage(userMessage);
     const response = await result.response;
-    return response.text();
+    const content = response.candidates[0].content;
+    const text = content.parts.map(({ text }) => text).join('');
+    const functionCall = content.parts[0].functionCall;
+
+    if (functionCall) {
+      const { name, args } = functionCall;
+      const fnResponse = functions[name](args);
+      const newResult = await this.#chat.sendMessage([
+        {
+          functionResponse: {
+            name,
+            response: {
+              name,
+              response: fnResponse,
+            },
+          }, 
+        }
+      ]);
+      const newResponse = await newResult.response;
+      return newResponse.text();
+    } else if (text) {
+      return text;
+    }
   }
 }
 
